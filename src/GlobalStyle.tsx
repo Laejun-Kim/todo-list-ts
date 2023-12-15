a2914c669e7985222e1966baf49a25ab93d47315
Laejun Kim <49670820+Laejun-Kim@users.noreply.github.com>
2023-12-15 11:41:28 +0900

    Feat:input validation

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-        html, body, div, span, applet, object, iframe,
+        html, body, span, applet, object, iframe,
     h1, h2, h3, h4, h5, h6, p, blockquote, pre,
     a, abbr, acronym, address, big, cite, code,
     del, dfn, em, img, ins, kbd, q, s, samp,
@@ -10,8 +10,8 @@ const GlobalStyle = createGlobalStyle`
     dl, dt, dd, ol, ul, li,
     fieldset, form, label, legend,
     table, caption, tbody, tfoot, thead, tr, th, td,
-    article, aside, canvas, details, embed, 
-    figure, figcaption, footer, header, hgroup, 
+    article, aside, canvas, details, embed,
+    figure, figcaption, footer, header, hgroup,
     menu, nav, output, ruby, section, summary,
     time, mark, audio, video {
         margin: 0;
@@ -22,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
         vertical-align: baseline;
     }
     /* HTML5 display-role reset for older browsers */
-    article, aside, details, figcaption, figure, 
+    article, aside, details, figcaption, figure,
     footer, header, hgroup, menu, nav, section {
         display: block;
     }
